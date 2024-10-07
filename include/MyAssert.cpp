5587f119afd3e6df4066324446ef64188f6b3870
Yan103 <101476599+Yan103@users.noreply.github.com>
2024-10-07 23:15:55 +0300

    Add files via upload

---
+++ include/MyAssert.cpp
@@ -0,0 +1,24 @@
+/*!
+    \file
+    File with the declaration of the my_assert function
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Default.h"
+
+/*!
+    @brief Function that, in case of an error, prints where it occurred and terminates the program
+    \param [in]  condition - the condition that needs to be checked
+    \param [in] text_error - error description text
+    \param [in]       file - the name of the file where the error occurred
+    \param [in]       func - the name of the function where the error occurred
+    \param [in]       line - the number of the line where the error occurred
+*/
+void my_assert(bool condition, const char *text_error, const char *file, const char *func, int line) {
+    if (!(condition)) {
+        fprintf(stderr, RED("%s in: %s -> %s -> %d\n"), text_error, file, func, line);
+        abort();
+    }
+}
